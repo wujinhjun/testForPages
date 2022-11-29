b9ffd6f83c43f811a8a59f23e89f3ebd7ce513b6
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-11-29 22:49:00 +0800

    feat: display word

--- src/sketch/sketch.ts
+++ src/sketch/sketch.ts
@@ -1,19 +1,129 @@
 import p5 from "p5";
+import { generateRandomLocation } from "./ControlFunction";
+import Particle from "./Particles";
 
-const mySketch = (parentElement: HTMLElement, widthSize = 300, heightSize = 300) => (sketch: p5) => {
 
-    sketch.setup = () => {
-        sketch.createCanvas(widthSize, heightSize);
-        sketch.background(0, 0, 0);
+const mySketch = (parentElement: HTMLElement) => (p: p5) => {
+    let particles: Particle[] = [];
+    let pixelSteps: number = 9;
+    let words: string[] = [];
+    let wordIndex: number = 0;
+    let fontName: string = "Arial";
+    let bgColor: p5.Color = p.color(20, 100);
+
+    const displayWord = (word: string, sketch: p5) => {
+        let pg: p5.Graphics = sketch.createGraphics(sketch.width, sketch.height);
+        pg.background(0);
+        pg.fill(255);
+        pg.textSize(150);
+        pg.textAlign("center", "center");
+        pg.textFont(fontName);
+        pg.text(word, sketch.width / 2, sketch.height / 2);
+        pg.loadPixels();
+
+        let newColor: p5.Color = sketch.color(sketch.random(255), sketch.random(255), sketch.random(255));
+
+        let particleCount: number = particles.length;
+        let particleIndex: number = 0;
+
+        let coordsIndexes: number[] = [];
+
+        for (let i = 0; i < (sketch.width * sketch.height); i += pixelSteps) {
+            coordsIndexes.push(i);
+        }
+
+        // console.log(`coordsIndexes: ${coordsIndexes.length}`);
+
+
+
+
+        for (let i = 0; i < coordsIndexes.length; i++) {
+            let randomIndex = Math.floor(sketch.random(0, coordsIndexes.length));
+            let coordIndex = coordsIndexes[randomIndex];
+            coordsIndexes.splice(randomIndex, 1);
+            // console.log(`coordsIndexes new: ${coordsIndexes.length}`);
+            // console.log(coordIndex);
+
+            if (pg.pixels[coordIndex * 4] !== 0) {
+                // console.log(particleCount);
+                let x = coordIndex % sketch.width;
+                let y = coordIndex / sketch.width;
+                // console.log(`x: ${x}, y: ${y}`);
+
+                let newParticle = new Particle(sketch);
+
+                if (particleIndex < particleCount) {
+                    newParticle = particles[particleIndex];
+                    newParticle.isKilled = false;
+                    particleIndex += 1;
+                } else {
+                    let randomLocation = generateRandomLocation(sketch.width / 2, sketch.height / 2, (sketch.width + sketch.height) / 2, sketch);
+                    newParticle.location.x = randomLocation.x;
+                    newParticle.location.y = randomLocation.y;
+
+                    newParticle.maxSpeed = sketch.random(2, 5);
+                    newParticle.maxForce = newParticle.maxSpeed * 0.025;
+                    newParticle.particleSize = sketch.random(3, 6);
+                    newParticle.colorBlendRate = sketch.random(0.0025, 0.03);
+
+                    particles.push(newParticle);
+                }
+                newParticle.startColor = sketch.lerpColor(newParticle.startColor, newParticle.targetColor, newParticle.colorWeight);
+                newParticle.targetColor = newColor;
+                newParticle.colorWeight = 0;
+
+                newParticle.target.x = x;
+                newParticle.target.y = y;
+            }
+        }
+
+        console.log(`particleCount: ${particles.length}`);
+
+        if (particleIndex < particleCount) {
+            for (let i = particleIndex; i < particleCount; i++) {
+                let p = particles[i];
+                p.kill();
+            }
+        }
     }
 
-    sketch.draw = () => {
-        sketch.clear(255, 255, 255, 0);
-        sketch.background(0, 0, 0);
-        sketch.fill(255);
-        sketch.rectMode("center");
-        sketch.rect(widthSize / 2, heightSize / 2, 300, 300);
+    const controlPlay = () => {
+        console.log(particles.length);
     }
+
+    p.setup = () => {
+        p.frameRate(30);
+        p.createCanvas(parentElement.offsetWidth, 300);
+        p.background(0, 0, 0);
+        // words.push("why");
+        words.push("落日西风吹尽")
+
+        displayWord(words[wordIndex], p);
+    }
+
+
+
+    p.draw = () => {
+
+        // p.clear(255, 255, 255, 0);
+        p.background(bgColor);
+        for (let i = particles.length - 1; i >= 0; i--) {
+            let particle = particles[i];
+            particle.run();
+
+            if (particle.isKilled) {
+                if (particle.location.x < 0 || particle.location.x > p.width || particle.location.y < 0 || particle.location.y > p.height) {
+                    particles.splice(i, 1);
+                }
+            }
+        }
+
+    }
+
+    p.mouseClicked = () => {
+        controlPlay();
+    }
+
 }
 
 export default mySketch;
