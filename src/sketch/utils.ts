df071f7014f84edac4c1e23f2f468f0abe03ff0c
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-11-29 22:50:03 +0800

    feat: utils

---
+++ src/sketch/utils.ts
@@ -0,0 +1,13 @@
+import p5 from "p5";
+
+export const generateRandomLocation = (_x: number, _y: number, _mag: number, sketch: p5): p5.Vector => {
+    let randomDirection = sketch.createVector(sketch.random(0, sketch.width), sketch.random(0, sketch.height));
+
+    let location: p5.Vector = sketch.createVector(_x, _y);
+    location.sub(randomDirection);
+    location.normalize();
+    location.mult(_mag);
+    location.add(_x, _y);
+
+    return location;
+}
