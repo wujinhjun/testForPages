aca0a4f0b43bbc84a384c9d4fc8dc30a2d66a3ff
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-05 23:47:09 +0800

    chore: config the host pic

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                port: '',
+                pathname: '/wujinhjun/wujinhjun-pic/main/**',
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
