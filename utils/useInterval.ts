f478cdf5b85ebb718234110e4d7f807e571a1a64
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-06 23:22:27 +0800

    feat: packaging the hooks for interval

---
+++ utils/useInterval.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from "react";
+
+const useInterval = (callback: () => void, delay: null | number) => {
+    const refIntervalId = useRef<ReturnType<typeof setInterval> | null>(null);
+    const refSavedCallback = useRef<() => void>();
+
+    const setupInterval = () => {
+        if (delay !== null) {
+            refIntervalId.current = setInterval(() => {
+                refSavedCallback.current!();
+            }, delay);
+        }
+    }
+
+    const cleanupInterval = () => {
+        refIntervalId.current && clearInterval(refIntervalId.current);
+
+    }
+    const resetInterval = () => {
+        cleanupInterval();
+        setupInterval();
+    }
+
+    useEffect(() => {
+        refSavedCallback.current = callback;
+    }, [callback])
+
+    useEffect(() => {
+        setupInterval();
+        return cleanupInterval;
+    }, [delay]);
+
+    return resetInterval;
+}
+
+export default useInterval;
