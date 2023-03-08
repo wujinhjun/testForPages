558d3bfcbe9f2a238dc8ba023119230fd2a03623
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-08 22:55:35 +0800

    feat: import home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
-import Header from "@/components/Header";
-import Layout from "@/components/Layou";
 import Banner from "@/components/Banner";
+import HomeContent from "@/components/HomeContent";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Wujinhjun-website</title>
+        <title>wujinhjun-website</title>
       </Head>
 
       <main className={styles.main}>
         <Banner />
+        <HomeContent />
       </main>
     </>
   );
