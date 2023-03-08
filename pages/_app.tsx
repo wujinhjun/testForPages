edeeb5ece7acc1d40ce0012fed792cccebd63623
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-08 22:55:16 +0800

    fix: optimize the header display

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layou";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const location = useRouter().pathname;
+
   return (
-    <Layout>
+    <Layout Poem={location === "/poem"}>
       <Component {...pageProps} />
     </Layout>
   );
