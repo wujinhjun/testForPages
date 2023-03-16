e895d370fb086f368e02c14da3c750a92320165e
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-16 15:49:59 +0800

    fix: update the assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-    reactStrictMode: true,
-    assetPrefix: isProduction ? "./" : undefined,
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                port: '',
-                pathname: '/wujinhjun/wujinhjun-pic/main/**',
-            },
-        ],
-    },
-}
+  reactStrictMode: true,
+  assetPrefix: "./",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/wujinhjun/wujinhjun-pic/main/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
