5492c2bce987dc596e839f9e1ef8547e51bd41a7
p_kunwwei <p_kunwwei@tencent.com>
2023-07-19 15:16:00 +0800

    style: format

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,37 +1,34 @@
-import Head from "next/head";
-import Header from "./Header";
-import styles from "./Layout.module.scss";
-import clsx from "clsx";
+import Head from 'next/head'
+import Header from './Header'
+import styles from './Layout.module.scss'
+import clsx from 'clsx'
 
 interface Props {
-  children: React.ReactNode;
-  Poem?: boolean;
+  children: React.ReactNode
+  poem?: boolean
 }
 
-export default function Layout({ children, Poem }: Props) {
+export default function Layout({ children, poem }: Props) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
         <meta
-          name="description"
-          content="The webpage for wujinhjun to display"
+          name='description'
+          content='The webpage for wujinhjun to display'
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <section className={styles.header_section}>
-        <Header Poem={Poem} />
-      </section>
 
       <>
         {children}
         <div
           className={clsx({
-            [styles.back_triangle]: !Poem,
-            [styles.back_triangle_2]: Poem,
+            [styles.back_triangle]: !poem,
+            [styles.back_triangle_2]: poem,
           })}
         ></div>
       </>
     </>
-  );
+  )
 }
