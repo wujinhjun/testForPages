4e39f3bf3a82201ed186a2d8bfa331e5546ccb59
p_kunwwei <p_kunwwei@tencent.com>
2023-07-21 00:12:12 +0800

    chore: config the tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js,jsx,tsx}',
+    './pages/**/*.{html,js,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
