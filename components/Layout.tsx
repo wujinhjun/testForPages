fd09c0e295bc2a3f54d16078c08d246ddbda94c0
p_kunwwei <p_kunwwei@tencent.com>
2023-07-21 16:37:51 +0800

    refactor: migrate the head from _document to _app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,25 +11,13 @@ interface Props {
 export default function Layout({ children, poem }: Props) {
   return (
     <>
-      <Head>
-        <link rel='icon' href='/favicon.ico' />
-        <meta
-          name='description'
-          content='The webpage for wujinhjun to display'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='viewport' content='viewport-fit=cover' />
-      </Head>
-
-      <>
-        {children}
-        <div
-          className={clsx({
-            [styles.back_triangle]: !poem,
-            [styles.back_triangle_2]: poem,
-          })}
-        ></div>
-      </>
+      {children}
+      <div
+        className={clsx({
+          [styles.back_triangle]: !poem,
+          [styles.back_triangle_2]: poem,
+        })}
+      ></div>
     </>
   )
 }
