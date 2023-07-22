a0c38a26c217b2c5e1608ac7c52e087330c14d4c
p_kunwwei <wujinhjun@outlook.com>
2023-07-22 23:07:47 +0800

    feat: update the banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,73 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.scss'
-import HeaderStyles from '@/styles/Header.module.scss'
-import Banner from '@/components/Banner'
 import HomeContent from '@/components/HomeContent'
 import type { IHome } from '@/utils/types'
 import { getHomeContentData } from '@/utils/content'
 import Header from '@/components/Header'
 import useScrollTop from '@/hooks/useScrollTop'
+import { useEffect, useMemo, useRef, useState } from 'react'
+
+const animationDurationTime = 500
 
 export default function Home({ introduction, project }: IHome) {
+  const [currentIndex, setCurrentIndex] = useState(1)
+  const [previousIndex, setPreviousIndex] = useState(currentIndex)
+  const [slideDirection, setSlideDirection] = useState<
+    'negative' | 'positive' | null
+  >(null)
+  const [switching, setSwitching] = useState(false)
+  const animationTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+
+  const bannerSize = useMemo(() => {
+    return introduction.length
+  }, [introduction])
+
   const scrollTop = useScrollTop()
+
+  const getIndex = (index: number): number => {
+    return (bannerSize + index) % bannerSize
+  }
+
+  const slideTo = ({
+    targetIndex,
+    isNegative = false,
+  }: {
+    targetIndex: number
+    isNegative?: boolean
+  }) => {
+    setSwitching(true)
+    setCurrentIndex(targetIndex)
+    setPreviousIndex(currentIndex)
+    setSlideDirection(isNegative ? 'negative' : 'positive')
+
+    animationTimerRef.current = setTimeout(() => {
+      setSwitching(false)
+      animationTimerRef.current = null
+    }, animationDurationTime)
+  }
+
+  const getLocation = (index: number): string => {
+    if (slideDirection && switching) {
+      switch (index) {
+        case previousIndex:
+          return `${slideDirection} prev`
+        case currentIndex:
+          return `${slideDirection} next`
+        default:
+          return ''
+      }
+    } else if (!switching && index === currentIndex) {
+      return 'current'
+    } else {
+      return ''
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      animationTimerRef.current && clearInterval(animationTimerRef.current)
+    }
+  }, [])
 
   return (
     <>
@@ -21,29 +79,52 @@ export default function Home({ introduction, project }: IHome) {
       </section>
       <main className={styles.main}>
         {/* <Banner contents={introduction} /> */}
-        <section className={`${styles.banner} flex mb-16`}>
-          <div className={styles.banner_wrapper}>
-            <div className={styles.banner_img_container}>
-              <div className={styles.banner_img_background}>
-                <img src={introduction[0].src} alt='' />
+        <section className={`${styles.banner_container}`}>
+          <section className={`${styles.banner} flex mb-16`}>
+            <div className={styles.banner_wrapper}>
+              <div className={styles.banner_img_container}>
+                {introduction.map((item, index) => (
+                  <div
+                    className={`${styles.banner_img_background} ${
+                      styles[getLocation(index)]
+                    } ${getLocation(index)}`}
+                    style={{ animationDuration: `${animationDurationTime}ms` }}
+                    key={`${index}`}
+                    aria-hidden={index === currentIndex}
+                  >
+                    <img src={item.src} alt={`banner picture ${1}`} />
+                  </div>
+                ))}
+              </div>
+              <div
+                className={`${styles.banner_button_group} flex justify-center gap-6`}
+              >
+                {introduction.map((item, index) => {
+                  return (
+                    <button
+                      key={`button-${index}-${item.id}`}
+                      open={index === currentIndex}
+                      onClick={() =>
+                        slideTo({
+                          targetIndex: index,
+                          isNegative: index < currentIndex,
+                        })
+                      }
+                    />
+                  )
+                })}
               </div>
             </div>
-            <div className={`flex justify-center gap-6 pt-6`}>
-              <button></button>
-              <button></button>
-              <button></button>
+            <div className={styles.banner_text_container}>
+              <p className={styles.banner_text_header}>
+                {introduction[currentIndex].header}
+              </p>
+              <p className={styles.banner_text_content}>
+                {introduction[currentIndex].content}
+              </p>
             </div>
-          </div>
-          <div className={styles.banner_text_container}>
-            <pre className={styles.banner_text_header}>
-              {introduction[0].header}
-            </pre>
-            <pre className={styles.banner_text_content}>
-              {introduction[0].content}
-            </pre>
-          </div>
+          </section>
         </section>
-
         <HomeContent production={project} />
       </main>
     </>
