3b23c74e5194378f313872f6e32973a4b52a1815
p_kunwwei <wujinhjun@outlook.com>
2023-07-24 14:54:11 +0800

    refactor: update the poem module

--- pages/poem/index.tsx
+++ pages/poem/index.tsx
@@ -6,14 +6,20 @@ import { NextSeo } from 'next-seo'
 import type { IPoem, ICatalogs } from '@/utils/types'
 import Header from '@/components/Header'
 import useScrollTop from '@/hooks/useScrollTop'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
+import { useMemo } from 'react'
 
 export default function Poem({ allPostsData }: IPoem) {
   //   console.log(allPostsData);
   const scrollTop = useScrollTop()
 
-  const catalogs: ICatalogs[] = allPostsData.map((item) => {
-    return { id: item.id, title: item.title as string }
-  })
+  const catalogs: ICatalogs[] = useMemo(
+    () =>
+      allPostsData.map((item) => {
+        return { id: item.id, title: item.title as string }
+      }),
+    [allPostsData]
+  )
 
   return (
     <>
@@ -22,7 +28,29 @@ export default function Poem({ allPostsData }: IPoem) {
         <Header name='poem' />
       </section>
       <main className={styles.main}>
-        <PoemDisplay catalogs={catalogs} contents={allPostsData}></PoemDisplay>
+        <section className={'flex justify-between flex-row-reverse'}>
+          <Sidebar>
+            <Menu>
+              <SubMenu label={2021}>
+                <MenuItem>1</MenuItem>
+                <MenuItem>2</MenuItem>
+                <MenuItem>3</MenuItem>
+              </SubMenu>
+              <SubMenu label={2022}>
+                <MenuItem>1</MenuItem>
+                <MenuItem>2</MenuItem>
+                <MenuItem>3</MenuItem>
+              </SubMenu>
+              <SubMenu label={2023}>
+                <MenuItem>1</MenuItem>
+                <MenuItem>2</MenuItem>
+                <MenuItem>3</MenuItem>
+              </SubMenu>
+            </Menu>
+          </Sidebar>
+        </section>
+
+        {/* ;<PoemDisplay catalogs={catalogs} contents={allPostsData}></PoemDisplay> */}
       </main>
     </>
   )
