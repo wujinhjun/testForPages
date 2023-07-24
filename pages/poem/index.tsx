9e64e607813c4967a026dd323d5bab8ad370eb0b
p_kunwwei <wujinhjun@outlook.com>
2023-07-25 01:27:25 +0800

    feat: update the catalog

--- pages/poem/index.tsx
+++ pages/poem/index.tsx
@@ -3,15 +3,39 @@ import PoemDisplay from '@/components/PoemDisplay'
 import styles from '@/styles/Home.module.scss'
 import { NextSeo } from 'next-seo'
 
-import type { IPoem, ICatalogs } from '@/utils/types'
+import type { IPoem, ICatalogs, IPoemHtml, IPoemCard } from '@/utils/types'
 import Header from '@/components/Header'
 import useScrollTop from '@/hooks/useScrollTop'
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
 import { useMemo } from 'react'
+import group from '@/utils/group'
+import {
+  SideMenu,
+  MenuItem as MyMenuItem,
+  SubMenu as MySubMenu,
+} from '@/components/SideMenu'
+
+const PoemCard = ({ title, content, id }: IPoemCard) => {
+  return (
+    <section className={styles.poemContainer} id={id}>
+      <div className={styles.poemTitleWrapper}>
+        <div className={styles.poemTitle}>{title}</div>
+      </div>
+      <div className={styles.poemContentWrapper}>
+        <div
+          className={styles.poemContent}
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+      </div>
+    </section>
+  )
+}
 
 export default function Poem({ allPostsData }: IPoem) {
   //   console.log(allPostsData);
   const scrollTop = useScrollTop()
+  const groupedPoem = group<IPoemHtml>(allPostsData, 'year')
+  console.log(groupedPoem)
 
   const catalogs: ICatalogs[] = useMemo(
     () =>
@@ -28,29 +52,41 @@ export default function Poem({ allPostsData }: IPoem) {
         <Header name='poem' />
       </section>
       <main className={styles.main}>
-        <section className={'flex justify-between flex-row-reverse'}>
-          <Sidebar>
-            <Menu>
-              <SubMenu label={2021}>
-                <MenuItem>1</MenuItem>
-                <MenuItem>2</MenuItem>
-                <MenuItem>3</MenuItem>
-              </SubMenu>
-              <SubMenu label={2022}>
-                <MenuItem>1</MenuItem>
-                <MenuItem>2</MenuItem>
-                <MenuItem>3</MenuItem>
-              </SubMenu>
-              <SubMenu label={2023}>
-                <MenuItem>1</MenuItem>
-                <MenuItem>2</MenuItem>
-                <MenuItem>3</MenuItem>
-              </SubMenu>
-            </Menu>
-          </Sidebar>
-        </section>
+        <section className='flex justify-between'>
+          <section className={styles.poems}>
+            {allPostsData.map((poem) => {
+              return (
+                <PoemCard
+                  key={poem.id}
+                  id={poem.id}
+                  title={poem.title}
+                  content={poem.content}
+                />
+              )
+            })}
+          </section>
 
-        {/* ;<PoemDisplay catalogs={catalogs} contents={allPostsData}></PoemDisplay> */}
+          <div className={styles.side_bar}>
+            <SideMenu>
+              {Object.keys(groupedPoem).map((key) => {
+                return (
+                  <MySubMenu chapter={key} key={`poem-submenu-${key}`}>
+                    {groupedPoem[key].map((poemItem) => {
+                      return (
+                        <MyMenuItem
+                          key={`${key}-poem-${poemItem.id}`}
+                          href={`#${poemItem.id}`}
+                        >
+                          {poemItem.title}
+                        </MyMenuItem>
+                      )
+                    })}
+                  </MySubMenu>
+                )
+              })}
+            </SideMenu>
+          </div>
+        </section>
       </main>
     </>
   )
