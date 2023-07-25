aa82f583fe07f0326c499492bec9cf170be36426
p_kunwwei <wujinhjun@outlook.com>
2023-07-25 15:52:27 +0800

    feat: scroll high light

--- pages/poem/index.tsx
+++ pages/poem/index.tsx
@@ -1,15 +1,19 @@
-import { getAllPostDataContent, getPostsIDs } from '@/lib/poemLib'
-import PoemDisplay from '@/components/PoemDisplay'
-import styles from '@/styles/Home.module.scss'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { useThrottleFn } from 'ahooks'
 import { NextSeo } from 'next-seo'
 
-import type { IPoem, ICatalogs, IPoemHtml, IPoemCard } from '@/utils/types'
 import Header from '@/components/Header'
-import useScrollTop from '@/hooks/useScrollTop'
-// import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
-import { useMemo } from 'react'
-import group from '@/utils/group'
 import { SideMenu, MenuItem, SubMenu } from '@/components/SideMenu'
+
+import useScrollTop from '@/hooks/useScrollTop'
+import group from '@/utils/group'
+import { getAllPostDataContent, getPostsIDs } from '@/lib/poemLib'
+
+import styles from '@/styles/Home.module.scss'
+
+import type { IPoem, IPoemHtml, IPoemCard } from '@/utils/types'
+
+const OFFSET_TOP = 96
 
 const PoemCard = ({ title, content, id }: IPoemCard) => {
   return (
@@ -28,18 +32,105 @@ const PoemCard = ({ title, content, id }: IPoemCard) => {
 }
 
 export default function Poem({ allPostsData }: IPoem) {
-  //   console.log(allPostsData);
   const scrollTop = useScrollTop()
   const groupedPoem = group<IPoemHtml>(allPostsData, 'year')
-  console.log(groupedPoem)
 
-  const catalogs: ICatalogs[] = useMemo(
-    () =>
-      allPostsData.map((item) => {
-        return { id: item.id, title: item.title as string }
-      }),
-    [allPostsData]
+  const anchorData: Record<
+    string,
+    Array<{ year: number; title: string; id: string }>
+  > = useMemo(() => {
+    const result: Record<
+      string,
+      Array<{ year: number; title: string; id: string }>
+    > = {}
+
+    Object.keys(groupedPoem).forEach((key) => {
+      result[String(key)] = groupedPoem[key].map((item) => {
+        return {
+          year: item.year,
+          id: item.id,
+          title: item.title,
+        }
+      })
+    })
+
+    return result
+  }, [groupedPoem])
+
+  const [activeAnchor, setActiveAnchor] = useState(
+    Object.values(anchorData)[0][0].id
   )
+
+  const { run: handleScroll } = useThrottleFn(
+    () => {
+      const domArray: Array<{ id: string; top: number }> = []
+
+      if (
+        document.documentElement.clientHeight + window.scrollY ===
+        document.body.scrollHeight
+      ) {
+        const dataFlat = Object.values(anchorData)
+
+        const lastId =
+          dataFlat[dataFlat.length - 1][
+            dataFlat[dataFlat.length - 1].length - 1
+          ].id
+
+        setActiveAnchor(lastId)
+      } else {
+        Object.keys(anchorData).forEach((key) => {
+          anchorData[key].forEach((item) => {
+            const dom = document.getElementById(item.id)
+            if (dom && dom.getBoundingClientRect().top > 0) {
+              domArray.push({
+                id: item.id,
+                top: Math.abs(dom.getBoundingClientRect().top - OFFSET_TOP),
+              })
+            }
+          })
+        })
+
+        if (domArray.length) {
+          domArray.sort((a, b) => a.top - b.top)
+          const { id } = domArray[0]
+          setActiveAnchor(id)
+        }
+      }
+    },
+    { wait: 150 }
+  )
+
+  const scrollAnchorRef = useRef<HTMLLIElement | null>(null)
+
+  useEffect(() => {
+    // 获取滚动锚点元素
+    const scrollAnchorElement = scrollAnchorRef.current
+
+    // 处理锚点点击事件
+    const handleAnchorClick = (event: any) => {
+      event.preventDefault()
+      const targetId = event.target.getAttribute('href')
+
+      if (targetId) {
+        const targetElement = document.getElementById(targetId.slice(1))
+        targetElement && targetElement.scrollIntoView()
+        setActiveAnchor(targetId.slice(1))
+      }
+    }
+
+    // 为锚点元素添加点击事件监听器
+    scrollAnchorElement &&
+      scrollAnchorElement.addEventListener('click', handleAnchorClick)
+
+    window && window.addEventListener('scroll', handleScroll)
+
+    // 在组件卸载时移除事件监听器
+    return () => {
+      scrollAnchorElement &&
+        scrollAnchorElement.removeEventListener('click', handleAnchorClick)
+      window && window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     <>
@@ -63,16 +154,22 @@ export default function Poem({ allPostsData }: IPoem) {
           </section>
 
           <div className={styles.side_bar}>
-            <SideMenu>
-              {Object.keys(groupedPoem).map((key) => {
+            <SideMenu forwardRef={scrollAnchorRef}>
+              {Object.keys(anchorData).map((key) => {
                 return (
-                  <SubMenu chapter={key} key={`poem-submenu-${key}`}>
-                    {groupedPoem[key].map((poemItem) => {
+                  <SubMenu
+                    chapter={key}
+                    key={`poem-submenu-${key}`}
+                    active={activeAnchor.slice(0, 4) == key}
+                  >
+                    {anchorData[key].map((poemItem) => {
                       return (
                         <MenuItem
                           key={`${key}-poem-${poemItem.id}`}
                           href={`#${poemItem.id}`}
-                          className={`active`}
+                          className={`${
+                            activeAnchor === poemItem.id ? 'active' : ''
+                          }`}
                         >
                           {poemItem.title}
                         </MenuItem>
