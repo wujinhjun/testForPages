78c0db611d519d6b22d8e35e2cf132af1b559f03
p_kunwwei <wujinhjun@outlook.com>
2023-09-01 10:28:16 +0800

    feat: update the poem

--- pages/poem/index.tsx
+++ pages/poem/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState, useLayoutEffect } from 'react'
 import { useThrottleFn } from 'ahooks'
 import { NextSeo } from 'next-seo'
+import { InfiniteLoader, List } from 'react-virtualized'
 
 import Header from '@/components/Header'
 import { SideMenu, MenuItem, SubMenu } from '@/components/SideMenu'
+import { LoaderRipple } from '@/components/Loader'
 
-import useScrollTop from '@/hooks/useScrollTop'
 import group from '@/utils/group'
 import { getAllPostDataContent, getPostsIDs } from '@/lib/poemLib'
 
@@ -24,16 +25,42 @@ const PoemCard = ({ title, content, id }: IPoemCard) => {
       <div className={styles.poemContentWrapper}>
         <div
           className={styles.poemContent}
-          dangerouslySetInnerHTML={{ __html: content }}
-        ></div>
+          dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
     </section>
   )
 }
 
+const remoteRowCount = 300
+const list = []
+
+function isRowLoaded({ index }) {
+  return !!list[index]
+}
+
+function loadMoreRows({ startIndex, stopIndex }) {
+  return fetch(
+    `path/to/api?startIndex=${startIndex}&stopIndex=${stopIndex}`
+  ).then((response) => {
+    // Store response data in list...
+  })
+}
+
+function rowRenderer({ key, index, style }) {
+  return (
+    <div key={key} style={style}>
+      {list[index]}
+    </div>
+  )
+}
+
 export default function Poem({ allPostsData }: IPoem) {
-  const scrollTop = useScrollTop()
+  //   const scrollTop = useScrollTop()
+  const [scrollTop, setScrollTop] = useState(0)
   const groupedPoem = group<IPoemHtml>(allPostsData, 'year')
+
+  const [poemData, setPoemData] = useState<IPoemHtml[]>([])
+  const [poemPage, setPoemPage] = useState(0)
 
   const anchorData: Record<
     string,
@@ -61,8 +88,14 @@ export default function Poem({ allPostsData }: IPoem) {
     Object.values(anchorData)[0][0].id
   )
 
+  const scrollAnchorRef = useRef<HTMLLIElement | null>(null)
+
   const { run: handleScroll } = useThrottleFn(
     () => {
+      // 计算Header
+      setScrollTop(window.scrollY)
+
+      // 计算锚点
       const domArray: Array<{ id: string; top: number }> = []
 
       if (
@@ -100,23 +133,21 @@ export default function Poem({ allPostsData }: IPoem) {
     { wait: 150 }
   )
 
-  const scrollAnchorRef = useRef<HTMLLIElement | null>(null)
+  // 处理锚点点击事件
+  const handleAnchorClick = (event: any) => {
+    event.preventDefault()
+    const targetId = event.target.getAttribute('href')
 
-  useEffect(() => {
+    if (targetId) {
+      const targetElement = document.getElementById(targetId.slice(1))
+      targetElement && targetElement.scrollIntoView()
+      setActiveAnchor(targetId.slice(1))
+    }
+  }
+
+  useLayoutEffect(() => {
     // 获取滚动锚点元素
     const scrollAnchorElement = scrollAnchorRef.current
-
-    // 处理锚点点击事件
-    const handleAnchorClick = (event: any) => {
-      event.preventDefault()
-      const targetId = event.target.getAttribute('href')
-
-      if (targetId) {
-        const targetElement = document.getElementById(targetId.slice(1))
-        targetElement && targetElement.scrollIntoView()
-        setActiveAnchor(targetId.slice(1))
-      }
-    }
 
     // 为锚点元素添加点击事件监听器
     scrollAnchorElement &&
@@ -140,19 +171,37 @@ export default function Poem({ allPostsData }: IPoem) {
       </section>
       <main className={styles.main}>
         <section className='flex justify-between'>
-          <section className={styles.poems}>
-            {allPostsData.map((poem) => {
-              return (
-                <PoemCard
-                  key={poem.id}
-                  id={poem.id}
-                  title={poem.title}
-                  content={poem.content}
+          <div id='poem-display-area'>
+            <section className={styles.poems}>
+              {allPostsData.map((poem, index) => {
+                return (
+                  <PoemCard
+                    key={`poem-card-${poem.id}`}
+                    id={poem.id}
+                    title={poem.title}
+                    content={poem.content}
+                  />
+                )
+              })}
+            </section>
+            <InfiniteLoader
+              isRowLoaded={isRowLoaded}
+              loadMoreRows={loadMoreRows}
+              rowCount={allPostsData.length}>
+              {({ onRowsRendered, registerChild }) => (
+                <List
+                  height={300}
+                  onRowsRendered={onRowsRendered}
+                  ref={registerChild}
+                  rowCount={remoteRowCount}
+                  rowHeight={20}
+                  rowRenderer={rowRenderer}
+                  width={300}
                 />
-              )
-            })}
-          </section>
-
+              )}
+            </InfiniteLoader>
+            ,
+          </div>
           <div className={styles.side_bar}>
             <SideMenu forwardRef={scrollAnchorRef}>
               {Object.keys(anchorData).map((key) => {
@@ -160,8 +209,7 @@ export default function Poem({ allPostsData }: IPoem) {
                   <SubMenu
                     chapter={key}
                     key={`poem-submenu-${key}`}
-                    active={activeAnchor.slice(0, 4) == key}
-                  >
+                    active={activeAnchor.slice(0, 4) == key}>
                     {anchorData[key].map((poemItem) => {
                       return (
                         <MenuItem
@@ -169,8 +217,7 @@ export default function Poem({ allPostsData }: IPoem) {
                           href={`#${poemItem.id}`}
                           className={`${
                             activeAnchor === poemItem.id ? 'active' : ''
-                          }`}
-                        >
+                          }`}>
                           {poemItem.title}
                         </MenuItem>
                       )
